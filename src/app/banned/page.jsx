a16c225c3c92a6dd23b3d0b00d9a8abd0ce51011
Learning React <himelsd117@gmail.com>
2025-11-12 04:20:41 +0600

    --allow-empty-message

--- src/app/banned/page.jsx
+++ src/app/banned/page.jsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function BannedComponent({
   title = "Account Permanently Banned",
