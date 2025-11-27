018f36e441452fc9ac38304a9652628c1b10801c
Learning React <himelsd117@gmail.com>
2025-11-28 04:14:09 +0600

    --allow-empty-message

--- src/app/auth/sign-up/page.jsx
+++ src/app/auth/sign-up/page.jsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { EyeIcon, EyeOffIcon } from "lucide-react";
+import { EyeIcon, EyeOffIcon, LoaderCircleIcon } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
